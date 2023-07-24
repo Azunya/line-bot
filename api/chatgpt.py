93e30b58f567af2f6f3e6389ff8e3e6275c1e1c3
jack_b_wu <jack_b_wu@trendmicro.com>
2023-07-25 01:27:25 +0800

    feat: alter config.

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -8,22 +8,35 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 class ChatGPT:
   def __init__(self):
     self.prompt = Prompt()
-    self.model = os.getenv('OPENAI_MODEL', default = 'text-davinci-003')
-    #self.model = os.getenv('OPENAI_MODEL', default = 'chatbot')
-    self.temperature = float(os.getenv('OPENAI_TEMPERATURE', default = 0))
+    self.model = os.getenv('OPENAI_MODEL', default = 'gpt-3.5-turbo')
+    self.temperature = float(os.getenv('OPENAI_TEMPERATURE', default = .6))
     self.frequency_penalty = float(os.getenv('OPENAI_FREQUENCY_PENALTY', default = 0))
-    self.presence_penalty = float(os.getenv('OPENAI_PRESENCE_PENALTY', default = 0.6))
+    self.presence_penalty = float(os.getenv('OPENAI_PRESENCE_PENALTY', default = .6))
     self.max_tokens = int(os.getenv('OPENAI_MAX_TOKENS', default = 240))
+    self.system_role_content = os.getenv('OPENAI_SYSTEM_ROLE_CONTENT', default = 'You are a cat.')
 
   def get_response(self):
-    response = openai.Completion.create(
+    response = openai.ChatCompletion.create(
       model=self.model,
-      prompt=self.prompt.generate_prompt(),
+      messages=[
+        {
+          'role': 'user',
+          'content': self.system_role_content,
+        }
+      ],
       temperature=self.temperature,
       frequency_penalty=self.frequency_penalty,
       presence_penalty=self.presence_penalty,
       max_tokens=self.max_tokens
     )
+    # response = openai.Completion.create(
+    #   model=self.model,
+    #   prompt=self.prompt.generate_prompt(),
+    #   temperature=self.temperature,
+    #   frequency_penalty=self.frequency_penalty,
+    #   presence_penalty=self.presence_penalty,
+    #   max_tokens=self.max_tokens
+    # )
     return response['choices'][0]['text'].strip()
 
   def add_msg(self, text):
